9d568f3521190070a47defe8c047cb4e30d0a338
LGuillet <laurent.guillet@u-cergy.fr>
2019-08-26 16:20:41 +0200

    Bug correction

--- enrolspecificsappuiadmin.php
+++ enrolspecificsappuiadmin.php
@@ -55,13 +55,8 @@ foreach ($listcourses as $course) {
 
     $contextcourse = context_course::instance($course->id);
 
-    print_object($roleappuiadmin);
-    print_object($contextcourse);
-
     if (!$DB->record_exists('role_assignments',
             array('roleid' => $roleappuiadmin->id, 'contextid' => $contextcourse->id))) {
-
-        print_object("Test");
 
         $user1 = $DB->get_record('user', array('username' => 'fangard', 'idnumber' => 6277));
 
